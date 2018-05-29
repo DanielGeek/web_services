dd23ef51cb5f46a93efe8d64c3bd11ed2bcedc51
DanielGeek <daniel30081990@gmail.com>
2018-05-29 02:10:23 -0400

    Agregado jquery al top

--- server/excel.php
+++ server/excel.php
@@ -6,9 +6,10 @@ $result = mysqli_query($connect, $sql);
 <html>  
  <head>  
   <title>Export MySQL data to Excel in PHP</title>  
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" /> 
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>   
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
+  
  </head>  
  <body>  
   <div class="container">  
